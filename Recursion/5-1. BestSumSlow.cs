0305f3e304eee386af0ec4c7fce5ff187b4ae922
yoponchik <57009810+yoponchik@users.noreply.github.com>
2022-03-04 01:57:41 +0900

    Update 5-1. BestSumSlow.cs
    fixed bug

--- Recursion/5-1. BestSumSlow.cs
+++ Recursion/5-1. BestSumSlow.cs
@@ -1,46 +1,39 @@
 ﻿class BestSumSlow
 {
 
-    static int[] BestSum(int targetSum, int[] numbers)
-    {
+    static int[] BestSum(int targetSum, int[] numbers){
 
-        int[] empty = new int[] { };
+        int[] empty = Array.Empty<int>();
 
         if (targetSum == 0) { return empty; }
         if (targetSum < 0) return null;
 
-        int[] shortestCombination = new int[] { };
+        int[] shortestCombination = Array.Empty<int>();
 
-        foreach (var num in numbers)
-        {
+        foreach (var num in numbers){
             var remainder = targetSum - num;
             int[] remainderCombination = BestSum(remainder, numbers);
 
-            if (remainderCombination != Array.Empty<int>())
-            {
+            if (remainderCombination != null){
                 var combination = remainderCombination.Append(num).ToArray();
-                if (shortestCombination == Array.Empty<int>() || combination.Length < shortestCombination.Length)
-                {
+                if (shortestCombination == Array.Empty<int>() || combination.Length < shortestCombination.Length){
                     shortestCombination = combination;
                 }
             }
         }
-
         return shortestCombination;
     }
 
-    static void Main(string[] args)
+    static void Main()
     {
-        int[] numbers2 = new int[] { 2, 3, 5 };
+        int[] numbers = new int[] { 2, 3, 5 };
 
-        int[] result = BestSum(8, numbers2);
+        int[] result = BestSum(8, numbers);
 
-        if (result != null)
-        {
+        if (result != null){
             Console.WriteLine(String.Join(",", result));
         }
-        else
-        {
+        else{
             Console.WriteLine("NULL");
         }
     }
