73d7460800245872d9de91305fa59f600e04e20b
yoponchik <57009810+yoponchik@users.noreply.github.com>
2022-03-04 03:58:14 +0900

    Upload CountConstruct Slow and Fast

---
+++ Recursion/7-1 CountConstructSlow.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+
+class CountConstructSlow
+{
+    static int CountConstruct(string target, string[] wordBank)
+    {
+        if (target == "") { return 1; }
+
+        var totalCount = 0;
+
+        foreach (var word in wordBank)
+        {
+            if (target.IndexOf(word) == 0)
+            {
+                var numWaysForRest = CountConstruct(target.Substring(word.Length), wordBank);
+                totalCount += numWaysForRest;
+            }
+        }
+
+        return totalCount;
+
+    }
+
+    static void Main()
+    {
+        string[] wordBank = new string[] { "p", "ur", "ple", "u", "p", "urple" };
+        Console.WriteLine(CountConstruct("purple", wordBank));
+    }
+}
---
+++ Recursion/7-2 CountConstructFast.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class CountConstructFast
+{
+
+    static Dictionary<string, int> _memo = new Dictionary<string, int>();           //need to have static or else "An object reference is required for the non-static field, method,etc"
+
+    static int CountConstruct(string target, string[] wordBank)
+    {
+
+        if (_memo.ContainsKey(target)) { return _memo[target]; }
+        if (target == "") { return 1; }
+
+        var totalCount = 0;
+
+        foreach (var word in wordBank)
+        {
+            if (target.IndexOf(word) == 0)
+            {
+                var numWaysForRest = CountConstruct(target.Substring(word.Length), wordBank);
+                totalCount += numWaysForRest;
+            }
+        }
+
+        _memo[target] = totalCount;
+        return totalCount;
+    }
+
+    static void Main()
+    {
+        string[] wordBank = new string[] { "s", "n", "i", "de", "d", "e", "sni" };
+
+        Console.WriteLine(CountConstruct("snide", wordBank));
+    }
+}
