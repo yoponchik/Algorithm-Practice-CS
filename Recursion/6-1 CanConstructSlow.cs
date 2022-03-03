86f9b6b00c034a3dd92c423905c7ca6f976e505d
yoponchik <57009810+yoponchik@users.noreply.github.com>
2022-03-04 03:40:35 +0900

    Upload CanConstruct Slow and Fast

---
+++ Recursion/6-1 CanConstructSlow.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class CanConstructSlow { 
+    static bool CanConstruct(string target, string[] wordBank){
+        if (target == "") { return true; }
+
+        foreach (var word in wordBank) {
+            if (target.IndexOf(word) == 0) {
+                var suffix = target.Substring(word.Length);
+                if (CanConstruct(suffix, wordBank) == true) { 
+                    return true; 
+                }
+            }
+        }
+
+        return false;
+    }
+
+    static void Main(string[] args) {
+
+        string[] wordBank = new string[] { "ab", "d", "ef" };
+        Console.WriteLine(CanConstruct("abcdef", wordBank));
+    }
+}
