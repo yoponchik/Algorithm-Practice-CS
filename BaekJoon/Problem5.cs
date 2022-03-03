88a7704411fb98e730f3840fbc227184563619f2
yoponchik <57009810+yoponchik@users.noreply.github.com>
2022-03-03 13:26:21 +0900

    BaekJoon Update

---
+++ BaekJoon/Problem5.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+//https://funfunhanblog.tistory.com/401https://funfunhanblog.tistory.com/401
+#region Problem5
+class Problem5
+{
+	public static void Main(string[] args)
+	{
+		int[] array = new int[20000001];
+		int n = int.Parse(Console.ReadLine());
+		string[] nstr = Console.ReadLine().Split(' ');
+
+		int m = int.Parse(Console.ReadLine());
+		string[] mstr = Console.ReadLine().Split(' ');
+		int nIdx = 10000000;
+
+		StringBuilder stringBuilder = new StringBuilder();
+		for (int i = 0; i < nstr.Length; i++)
+		{
+			array[nIdx + int.Parse(nstr[i])]++;
+
+		}
+		for (int i = 0; i < mstr.Length; i++)
+		{
+			stringBuilder.Append(array[nIdx + int.Parse(mstr[i])] + " ");
+		}
+
+		Console.WriteLine(stringBuilder.ToString());
+
+
+	}
+}
+
+#endregion Problem5
