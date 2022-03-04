76e361354cd365f7f5dd8c30986f1114d6cc9753
yoponchik <57009810+yoponchik@users.noreply.github.com>
2022-03-04 23:04:10 +0900

    Revert "GridTraveller Notes and Pictures"
    This reverts commit acbf354da6fc4472bf72be46034ffaae5b6cc188.

--- Recursion/02. GridTraveller/2-1. GridTravellerSlow.cs
+++ Recursion/02. GridTraveller/2-1. GridTravellerSlow.cs
@@ -1,13 +1,11 @@
 ﻿using System;
 
 class GridTravellerSlow{
-
     static int GridTravel(int m, int n) {
         if (m == 1 && n == 1) return 1;
         if (m == 0 || n == 0) return 0;                             //remember there is an or
-        return GridTravel(m - 1, n) + GridTravel(m, n - 1);         //can either go down or right
+        return GridTravel(m - 1, n) + GridTravel(m, n - 1);         //can either go left or right
     }
-
     static void Main(string[] args) { 
         Console.WriteLine(GridTravel(1,1));
         Console.WriteLine(GridTravel(2,3));
--- Recursion/02. GridTraveller/2-2. GridTravellerFast.cs
+++ Recursion/02. GridTraveller/2-2. GridTravellerFast.cs
@@ -2,21 +2,20 @@
 
 class GridTravellerFast
 {
-    static Dictionary<Tuple<int, int>, long> _memo = new Dictionary<Tuple<int, int>, long>();   //Tuple type key
+    static Dictionary<Tuple<int, int>, long> _memo = new Dictionary<Tuple<int, int>, long>();
 
     static long GridTravel(int m, int n)
     {
-        Tuple<int, int> key;                        //declare a tuple key
-        key = Tuple.Create(m, n);                   //initialize/create a key
+        Tuple<int, int> key;
+        key = Tuple.Create(m, n);
 
-        if (_memo.ContainsKey(key)) { return _memo[key]; }          //check dictionary with tuple key, return if memoized
-        if (m == 1 && n == 1) return 1;                             //positive base case    
-        if (m == 0 || n == 0) return 0;                             //negative base case
+        if (_memo.ContainsKey(key)) { return _memo[key]; }
+        if (m == 1 && n == 1) return 1;
+        if (m == 0 || n == 0) return 0;
 
-        _memo[key] = GridTravel(m - 1, n) + GridTravel(m, n - 1);   //store memo
-        return _memo[key];                                          //return memoized
+        _memo[key] = GridTravel(m - 1, n) + GridTravel(m, n - 1);
+        return _memo[key];
     }
-
     static void Main(string[] args)
     {
         Console.WriteLine(GridTravel(1, 1));
