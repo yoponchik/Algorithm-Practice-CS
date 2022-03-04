82555495a5e8da2812fb356d6f49ca66bb539e91
yoponchik <57009810+yoponchik@users.noreply.github.com>
2022-03-05 03:04:15 +0900

    Update 5-1. BestSumSlow.cs

--- Recursion/05. BestSum/5-1. BestSumSlow.cs
+++ Recursion/05. BestSum/5-1. BestSumSlow.cs
@@ -1,26 +1,34 @@
-﻿class BestSumSlow
+﻿using System;
+
+class BestSumSlow
 {
 
-    static int[] BestSum(int targetSum, int[] numbers){
+    static int[] BestSum(int targetSum, int[] numbers)
+    {
 
         int[] empty = Array.Empty<int>();
 
         if (targetSum == 0) { return empty; }
         if (targetSum < 0) return null;
 
-        int[] shortestCombination = Array.Empty<int>();
+        //int[] shortestCombination = Array.Empty<int>();
+        int[] shortestCombination = null;
 
-        foreach (var num in numbers){
+        foreach (var num in numbers)
+        {
             var remainder = targetSum - num;
             int[] remainderCombination = BestSum(remainder, numbers);
 
-            if (remainderCombination != null){
+            if (remainderCombination != null)
+            {
                 var validCombination = remainderCombination.Append(num).ToArray();
-                if (shortestCombination == null || validCombination.Length < shortestCombination.Length){
+                if (shortestCombination == null || validCombination.Length < shortestCombination.Length)
+                {
                     shortestCombination = validCombination;
                 }
             }
         }
+
         return shortestCombination;
     }
 
@@ -30,10 +38,12 @@
 
         int[] result = BestSum(8, numbers);
 
-        if (result != null){
+        if (result != null)
+        {
             Console.WriteLine(String.Join(",", result));
         }
-        else{
+        else
+        {
             Console.WriteLine("NULL");
         }
     }
