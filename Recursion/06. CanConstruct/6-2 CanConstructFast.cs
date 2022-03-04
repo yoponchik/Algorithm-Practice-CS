f820b6c7466836ede0006ed1284584fe10c48f40
yoponchik <57009810+yoponchik@users.noreply.github.com>
2022-03-05 03:33:35 +0900

    Update 6-2 CanConstructFast.cs

--- Recursion/06. CanConstruct/6-2 CanConstructFast.cs
+++ Recursion/06. CanConstruct/6-2 CanConstructFast.cs
@@ -1,19 +1,15 @@
 ﻿using System;
 
-class CanConstructFast
-{
+class CanConstructFast{
 
     static Dictionary<string, bool> _memo = new Dictionary<string, bool>();
 
-    static bool CanConstruct(string target, string[] wordBank)
-    {
+    static bool CanConstruct(string target, string[] wordBank){
         if (_memo.ContainsKey(target)) { return _memo[target]; }
         if (target == "") { return true; }
 
-        foreach (var word in wordBank)
-        {
-            if (target.IndexOf(word) == 0)
-            {
+        foreach (var word in wordBank){
+            if (target.IndexOf(word) == 0){
                 var suffix = target.Substring(word.Length);
                 if (CanConstruct(suffix, wordBank))
                 {
