bfdc72691faeea70282bfcb325d4ba814d0e1e00
yoponchik <57009810+yoponchik@users.noreply.github.com>
2022-03-04 21:35:57 +0900

    Reorganized
    -added Fibonacci picture of tree

---
+++ Recursion/01. Fibonacci/1-1. FibonacciSlow.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class FibonacciSlow
+{
+    //Fibonacci function is basically getting the sum of the previous Fib (n-1) and the one before that(n-2)
+
+    static int FibSlow(int n)
+    {
+        if (n <= 2) { return 1; }                   //base case
+
+        return FibSlow(n - 1) + FibSlow(n - 2);     //if not basecase, recursion
+    }
+
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine(FibSlow(6));
+        Console.WriteLine(FibSlow(7));
+        Console.WriteLine(FibSlow(8));
+    }
+
+}
