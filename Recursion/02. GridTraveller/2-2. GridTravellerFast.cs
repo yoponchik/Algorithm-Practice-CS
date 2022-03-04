ebbf3f410d792a07ad17ed9e789cbc068688979d
yoponchik <57009810+yoponchik@users.noreply.github.com>
2022-03-04 23:04:27 +0900

    Revert "Revert "GridTraveller Notes and Pictures""
    This reverts commit 76e361354cd365f7f5dd8c30986f1114d6cc9753.

--- Recursion/02. GridTraveller/2-2. GridTravellerFast.cs
+++ Recursion/02. GridTraveller/2-2. GridTravellerFast.cs
@@ -2,20 +2,21 @@
 
 class GridTravellerFast
 {
-    static Dictionary<Tuple<int, int>, long> _memo = new Dictionary<Tuple<int, int>, long>();
+    static Dictionary<Tuple<int, int>, long> _memo = new Dictionary<Tuple<int, int>, long>();   //Tuple type key
 
     static long GridTravel(int m, int n)
     {
-        Tuple<int, int> key;
-        key = Tuple.Create(m, n);
+        Tuple<int, int> key;                        //declare a tuple key
+        key = Tuple.Create(m, n);                   //initialize/create a key
 
-        if (_memo.ContainsKey(key)) { return _memo[key]; }
-        if (m == 1 && n == 1) return 1;
-        if (m == 0 || n == 0) return 0;
+        if (_memo.ContainsKey(key)) { return _memo[key]; }          //check dictionary with tuple key, return if memoized
+        if (m == 1 && n == 1) return 1;                             //positive base case    
+        if (m == 0 || n == 0) return 0;                             //negative base case
 
-        _memo[key] = GridTravel(m - 1, n) + GridTravel(m, n - 1);
-        return _memo[key];
+        _memo[key] = GridTravel(m - 1, n) + GridTravel(m, n - 1);   //store memo
+        return _memo[key];                                          //return memoized
     }
+
     static void Main(string[] args)
     {
         Console.WriteLine(GridTravel(1, 1));
